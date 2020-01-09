c1cb47d693988b4555ead2d4b9bfca4ae81018c3
Daryl Corbin <corbindac@gmail.com>
2020-01-09 21:48:22 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -41,8 +41,10 @@ end
 def matrix_lookup(matrix, row, column)
   # Given any matrix (array of arrays), a row index and a column index,
   # Return the matrix's content at that row and and column
-matrix[row][column]
-matrix
+  produce_storage_room = [
+      ["Grapefruit","Pineapple","Oranges","Watermelon","Eggplant"].find,
+      ["Strawberries", "Potatoes", "Grapes", "Avocadoes","Asparagus"].find
+    ]
 end
 
 def matrix_update(matrix, row, column, new_value)
